621de74a7c14f9a00604194b018b08ff7af9d3fd
Tiago Reis Romao <treis-ro@c2r1s4.42lisboa.com>
2024-01-22 15:23:02 +0000

    yea yea

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: treis-ro <treis-ro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:55:54 by treis-ro          #+#    #+#             */
-/*   Updated: 2024/01/22 15:21:18 by treis-ro         ###   ########.fr       */
+/*   Updated: 2024/01/22 15:22:54 by treis-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@ int ft_int_hex(int nbr, char c)
     int     size;
     
     k = nbr;
-    size = 0;
+    size = -1;
     if (k < 0)
     {
         size += ft_int_putchar('-');
